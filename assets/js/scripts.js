5a60ee94d7e21632b1aea74e01eee0b5a3eaf2df
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2025-11-08 01:09:19 +0600

    Final commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,13 @@
-$(document).ready(function(){
-	// Mobile Menu
-	// $('.logo a i').click(function(){
-	// 	$('.menu ul').slideToggle(1000);
-
-	// 	return false
-	// });
-
+$(document).ready(function(){ 
 	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
+	$('.slider-carousel').owlCarousel({
+		items:1,
+		loop:true,
+		nav:false,
+		dots:false,
+		autoplay:true,
+		margin:20,
+	});
 
 
 
